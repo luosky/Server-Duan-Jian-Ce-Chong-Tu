939ebd4d4ba8e4a9f4fd3baea0140d64fceee7ae
Luosky <luosky@gmail.com>
2024-05-28 04:51:02 +0000

    增加baidu asr

--- playground/baidu_asr.ts
+++ playground/baidu_asr.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const ACCESS_TOKEN = process.env['BAIDU_ACCESS_TOKEN']
   
 const CREATE_JOB_API = "https://aip.baidubce.com/rpc/2.0/aasr/v1/create"
+const GET_JOB_RESULT_API = "https://aip.baidubce.com/rpc/2.0/aasr/v1/query"
 
 
 export async function create_job_for_url(url) {
@@ -13,12 +14,49 @@ export async function create_job_for_url(url) {
     "rate": "8000",
     "speech_url":  url,
     "pid": "1134",
-    "format": "pcm"
+    // "channel" : 2,
+    "format": "wav"
+    
   }
 
   console.log('data', data)
   const response = await axios.post(post_url, data)  
-  console.log('Success:', response.data);
-  return response.data.task_id
+  console.log('response data:', response.data);
+  const result = response.data
+  if (result.task_id) {
+    return result.task_id
+  } else {
+    return "创建任务失败："+result.error_msg
+  }
+}
+
+
+export async function get_result_for_job(job_id) {
+
+  const post_url = GET_JOB_RESULT_API + "?access_token=" + ACCESS_TOKEN
+  // url = urlencode(url)
+  console.log('job_id', job_id)
+  const jobs = [job_id]
+  console.log('jobs', jobs)
+  const data = {
+    "task_ids": jobs
+  }
+
+  console.log('data', data)
+  const response = await axios.post(post_url, data)  
+  console.log('response data:', response.data);
+
+  const tasks = response.data.tasks_info
+  console.log(`tasks length : ${tasks.length}`)
+  const task = tasks[0]
+  const task_id = task.task_id
+  const status = task.task_status
+  if (status == "Success") {
+    const result = task.task_result.result
+    const detail_result = task.task_result.detailed_result
+    return { task_id,status,result,detail_result}
+  } else {
+    return {task_id,status}
+  }
   
 }
--- server.ts
+++ server.ts
@@ -1,8 +1,14 @@
 import express from 'express'
+import cors from 'cors';
+
 import { check_conflict_task ,auto_create_task_for_requirement} from './playground/check_conflict_task'
-import { create_job_for_url} from './playground/baidu_asr'
+import { create_job_for_url, get_result_for_job} from './playground/baidu_asr'
 
 const app = express()
+// 允许所有域进行跨域请求
+app.use(cors());
+// 解析 JSON 格式的请求体
+app.use(express.json());
 const port = 3000
 
 // http trigger
@@ -35,17 +41,29 @@ app.get('/auto_create_task_for_requirement',async(req,res) => {
 app.get('/', async (req, res) => {
 
   // await check_conflict_task();
-  res.send('hello world')
+  res.send('hello world111')
 });
 
-app.get('/create_baidu_asr_job', async (req, res) => {
-  const url = req.query.url
-
+app.post('/create_baidu_asr_job', async (req, res) => {
+  // const url = req.query.url
+  const {url} = req.body
+  console.log(`req : ${req}`)
+  console.log(`url : ${url}`)
   const task_id = await create_job_for_url(url);
   console.log('response : ' + task_id)
   const data = { 
     text : task_id //为了复用图片转文字的多维表格插件，这里输出还是以 text 为 key。  https://yellowduck.feishu.cn/docx/Q89SdbfZyoI50RxiwxlcTtP8nkb
   };
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(data);
+});
+
+app.post('/get_result_for_job', async (req, res) => {
+  // const url = req.query.url
+  const {job} = req.body
+  console.log(`job : ${job}`)
+  const data = await get_result_for_job(job);
+  console.log('return response data: ' + data)
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(data);
 });
